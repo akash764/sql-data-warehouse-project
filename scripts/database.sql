4905bd48a9e8451c19f145f8989cd91a8ca721db
Akash u <123756504+akash764@users.noreply.github.com>
2025-05-19 11:28:40 +0530

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -12,7 +12,7 @@ WARNING!
 Running this script will drop the entire 'DataWarehouse' database if it exists.
 All data in the database will be permanently deleted. Proceed with caution
 and ensure you have proper backups before running this script.
-/*
+*/
 
 drop database if exists datawarehouse;
 
