878093d320a56acdf3fc234ce8469c599bc0ecbe
Akash u <123756504+akash764@users.noreply.github.com>
2025-05-19 11:28:17 +0530

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -19,10 +19,48 @@ drop database if exists datawarehouse;
 
 create database Datawarehouse;
 
-use datawarehouse;
+create table bronze_crm_cust_info(
+id              INT,
+cst_key             VARCHAR(50),
+cst_firstname       VARCHAR(50),
+cst_lastname        VARCHAR(50),
+cst_marital_status  VARCHAR(50),
+cst_gndr            VARCHAR(50),
+cst_create_date     DATE);
+    
+create table bronze_crm_prd_info(
+prd_id int,
+prd_Key varchar(50),
+prd_nm varchar(50),
+prd_cost int,
+prd_line varchar(50),
+prd_start_dt datetime,
+prdend_dt datetime);
 
-create table bronze( id int);
 
-create table silver( id int);
+create table bronze_crm_sales_details(
+sls_ord_num varchar(50),
+sls_prd_key varchar(50),
+sls_cust_id int,
+sls_order_dt int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
+sls_quantity int,
+sls_price int);
 
-create table gold(id int);
+
+create table bronze_erp_loc_a101(
+cid varchar(50),
+cntry varchar(50));
+
+create table bronze_erp_cust_az12(
+cid varchar(50),
+bdate date,
+gen varchar(50));
+
+create table bronze_erp_px_cat_g1v2(
+id varchar(50),
+cat varchar(50),
+subcat varchar(50),
+maintenance varchar(50));
